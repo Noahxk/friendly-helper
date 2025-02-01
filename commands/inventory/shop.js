b1bbfbd13a60cbb6206c7ef7b8138cf5e3a79106
Noahxk <noahxk@outlook.com>
2025-02-01 17:35:10 +1100

    Shop.js uses json files. Added shop folder and files to resources folder

--- commands/inventory/shop.js
+++ commands/inventory/shop.js
@@ -1,5 +1,6 @@
 const profileModel = require('../../models/profileSchema');
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
 
 module.exports= {
 	data: new SlashCommandBuilder()
@@ -31,109 +32,52 @@ module.exports= {
 			console.log(err);
 		}
 
-        const avaliable_items = [
-            {
-                id: "police radio",
-                display_name: "Police Radio",
-                description: "A permanent object that gives you a 50% chance of halving the fine when you fail a robbery.",
-                price: 2000,
-                type: "item"
-            },
-            {
-                id: "padlock",
-                display_name: "Padlock",
-                description: "A one time use item that prevents you from being robbed.",
-                price: 200,
-                type: "item"
-            },
-            {
-                id: "black hole",
-                display_name: "Black Hole",
-                roleID: "1064413042491785306",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "prismatic",
-                display_name: "Prismatic",
-                roleID: "1064412486931058729",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "lunar",
-                display_name: "Lunar",
-                roleID: "1064414523802525807",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "solar",
-                display_name: "Solar",
-                roleID: "1064414464230830162",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "cosmic",
-                display_name: "Cosmic",
-                roleID: "1064416287930994760",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "demonic",
-                display_name: "Demonic",
-                roleID: "1065519733086093332",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "angelic",
-                display_name: "Angelic",
-                roleID: "1065519448351572028",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "vortex",
-                display_name: "Vortex",
-                roleID: "1064412888288211024",
-                price: 4000,
-                type: "cosmetic"
-            },
-            {
-                id: "vibrant",
-                display_name: "Vibrant",
-                roleID: "1064412548214046730",
-                price: 4000,
-                type: "cosmetic"
-            },
-        ];
-
         const shop_inventory = new Map();
-        avaliable_items.forEach(item =>{
-            shop_inventory.set(item.id, item);
+        const shop_inventory_array = [];
+
+        const item_type_folders = fs.readdirSync("resources/shop");
+        item_type_folders.forEach(folder => {
+            const item_json_files = fs.readdirSync(`resources/shop/${folder}/json`).filter(file => file.endsWith(".json"));
+            item_json_files.forEach(file => {
+                const file_object = require(`../../resources/shop/${folder}/json/${file}`);
+                shop_inventory.set(file_object.id, file_object);
+                shop_inventory_array.push(file_object);
+            });
         });
+
+        function create_inventory_object(item) {
+            const obj = {
+                id: item.id,
+                display_name: item.display_name,
+                type: item.type
+            }
+            if(item.type == "cosmetic") obj.roleID = item.roleID;
+            if(item.type == "redeemable") obj.function_file = item.function_file;
+            return obj;
+        }
 
         if(interaction.options.getSubcommand() == "buy") {
 
             let item_to_buy = interaction.options.getString("item");
+            item_to_buy = item_to_buy.toLowerCase().replace(" ", "_");
+            console.log(item_to_buy);
 
             if(shop_inventory.get(item_to_buy)) {
                 item_to_buy = shop_inventory.get(item_to_buy);
             } else return interaction.reply({content: "Invalid item: Does not exist"});
 
             if(profileData.coins - item_to_buy.price < 0) return interaction.reply({content: "You do not have enough money to buy that item."});
-            if(item_to_buy.type == "item") {
+
+            if(item_to_buy.type == "item" || item_to_buy.type == "redeemable") {
+
                 if(profileData.inventory.some(item => item.id == item_to_buy.id)) {
-                    return interaction.reply({content: "You already have one of these in your inventory."});
+                    return interaction.reply({content: "Error: You can only own one of this."});
                 } else {
                     const response = await profileModel.findOneAndUpdate({
                         userID: interaction.user.id,
                     }, {
                         $push: {
-                            inventory: item_to_buy
+                            inventory: create_inventory_object(item_to_buy)
                         }
                     });
 
@@ -144,17 +88,18 @@ module.exports= {
                             coins: -item_to_buy.price
                         }
                     });
-                    console.log(profileData.inventory.includes(item_to_buy.display_name))
                 } 
             } else if(item_to_buy.type == "cosmetic") {
+
                 if(profileData.cosmetics.some(cosmetic => cosmetic.id == item_to_buy.id)) {
-                    return interaction.reply({content: "You already have this cosmetic."});
+                    return interaction.reply({content: "Error: You can only own one of this."});
                 } else {
+
                     const response = await profileModel.findOneAndUpdate({
                         userID: interaction.user.id,
                     }, {
                         $push: {
-                            cosmetics: item_to_buy
+                            cosmetics: create_inventory_object(item_to_buy)
                         }
                     });
 
@@ -165,6 +110,7 @@ module.exports= {
                             coins: -item_to_buy.price
                         }
                     });
+
                 }
             }
 
@@ -185,22 +131,22 @@ module.exports= {
             
             let fieldText = [];
 
-            avaliable_items.forEach(item =>{
-                if(item.type == "item") {
+            shop_inventory_array.forEach(item =>{
+                if(item.type == "item" || item.type == "redeemable") {
 
-                // if item type
+                // if item or redeemable type
                 newEmbed.addFields(
                     {name: `__${item.display_name}__`, value: `
                         **Description:** ${item.description}
                         **Price:** ${item.price} :coin:
                     `});
 
-            } else if(item.type == "cosmetic") {
+                } else if(item.type == "cosmetic") {
 
                 // if cosmetic type
-                fieldText.push(`**${item.display_name}** - <&${item.roleID}>`)
+                    fieldText.push(`**${item.display_name}** - <@&${item.roleID}>`)
 
-            }
+                }
             });
 
             newEmbed.addFields({name: "__Cosmetics__", value: fieldText.join("\n")})
