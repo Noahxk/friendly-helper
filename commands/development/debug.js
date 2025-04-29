e9128be171c5a6f3bb74a09d39088a1a8121d787
Noahxk <noahxk@outlook.com>
2025-04-29 16:33:15 +1000

    Added component subcommand to debug command for testing of the new v2 components

--- commands/development/debug.js
+++ commands/development/debug.js
@@ -1,6 +1,8 @@
 const profileModel = require('../../models/profileSchema');
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, ContainerBuilder} = require('discord.js');
 const profileModelFetcher = require("../../models/fetchers/profileModelFetcher");
+const { V2ComponentBuilder, V2TextDisplay, V2ContainerBuilder, V2ActionRowBuilder, V2ButtonBuilder, V2Separator } = require("v2componentsbuilder");
+const { ButtonStyle } = require("discord.js");
 
 module.exports= {
 	data: new SlashCommandBuilder()
@@ -45,25 +47,28 @@ module.exports= {
 							.setDescription("The user to get the profile model of")
 							.setRequired(true)
 					)
+			)
+			.addSubcommand(subcommand =>
+				subcommand
+					.setName("component")
+					.setDescription("Test discord message component")
 			),
 	async execute(interaction, Discord, client, fetch, perm) {
 
 		switch(interaction.options.getSubcommand()) {
 			case "log":
-				if(perm < 2) return interaction.reply({content: "Error: You need at least permission level 2"});
 				const log_message = interaction.options.getString("message");
 				console.log(log_message);
 				interaction.reply({content: "Message Logged"});
 				break;
 			case "echo":
-				if(perm < 2) return interaction.reply({content: "Error: You need at least permission level 2"});
+				if(perm < 2) return interaction.reply({content: "Insufficient permissions"});
 				const echo_message = interaction.options.getString("message");
 				const echo_channel = interaction.options.getChannel("channel");
 				echo_channel.send({content: echo_message});
 				interaction.reply({content: "Message Echoed"});
 				break;
 			case "dbtest":
-
 				const profileData = await profileModelFetcher.fetch(interaction.user.id);
 
 				const newEmbed = new Discord.EmbedBuilder()
@@ -71,6 +76,72 @@ module.exports= {
 				.setTitle('DEBUG MESSAGE')
 				.setDescription(`\`\`\`${profileData}\`\`\``)
 				interaction.reply({embeds: [newEmbed]});
+				break;
+			case "component":
+				const components = new V2ComponentBuilder().addComponents([
+					new V2ContainerBuilder().setComponents([
+						new V2TextDisplay("Enter Code:"),
+						new V2Separator(),
+
+						new V2ActionRowBuilder()
+							.setComponents([
+								new V2ButtonBuilder()
+									.setLabel("7")
+									.setCustomId("7")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("8")
+									.setCustomId("8")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("9")
+									.setCustomId("9")
+									.setStyle(ButtonStyle.Secondary)
+							]),
+
+						new V2ActionRowBuilder()
+							.setComponents([
+								new V2ButtonBuilder()
+									.setLabel("4")
+									.setCustomId("4")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("5")
+									.setCustomId("5")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("6")
+									.setCustomId("6")
+									.setStyle(ButtonStyle.Secondary)
+							]),
+
+						new V2ActionRowBuilder()
+							.setComponents([
+								new V2ButtonBuilder()
+									.setLabel("1")
+									.setCustomId("1")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("2")
+									.setCustomId("2")
+									.setStyle(ButtonStyle.Secondary),
+								new V2ButtonBuilder()
+									.setLabel("3")
+									.setCustomId("3")
+									.setStyle(ButtonStyle.Secondary)
+							]),
+
+						new V2ActionRowBuilder()
+							.setComponents([
+								new V2ButtonBuilder()
+									.setLabel("0")
+									.setCustomId("0")
+									.setStyle(ButtonStyle.Secondary)
+						])
+					])
+				]);
+				interaction.reply(components.toJSON());
+
 				break;
 		}
 		console.log(`${interaction.user.username} used ${interaction.commandName}`);
